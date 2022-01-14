8a6975452a75a68bb2bf937aa75b6cac413952fd
Volkan Sarıhan <97304152+VolkanSarihan@users.noreply.github.com>
2022-01-14 19:15:08 +0300

    Update gamePractice.py

--- gamePractice.py
+++ gamePractice.py
@@ -15,11 +15,12 @@ def CannonDestroy(p, enemy):
     p['hp'] = p['hp'] * 0.9
     enemy['hp'] = enemy['hp'] * 0.8
     
-def CannonHeal(p):
+def CannonHeal(p,):
     if p['hp']>60:
         p['hp'] = 75
     else:
-        p['hp'] = p['hp'] + 15
+        p['hp'] = p['hp'] + 15 
+    
 
 def attack(p,enemy):
     enemy["hp"]= enemy["hp"]-p["power"]
@@ -30,9 +31,9 @@ characters = [
         'hp':250,
         'power':20,
         'skills': [
-            {'name': 'Basic Attack', 'trigger': attack},
-            {'name': 'Grenade', 'trigger': TerminatorGrenade},
-            {'name': 'Machine Gun', 'trigger': TerminatorMachineGun}
+            {'name': 'Basic Attack', 'trigger': attack,'type':'attack','cooldown':0,'cooldownLeft':0},
+            {'name': 'Grenade', 'trigger': TerminatorGrenade,'type':'attack','cooldown':2,'cooldownLeft':3},
+            {'name': 'Machine Gun', 'trigger': TerminatorMachineGun,'type':'attack','cooldown':3,'cooldownLeft':4}
         ]
     },
     {
@@ -40,9 +41,9 @@ characters = [
         'hp':500,
         'power':5,
         'skills': [
-            {'name': 'Basic Attack', 'trigger': attack},
-            {'name': 'Lava Lash', 'trigger': VolcanoLavaLash},
-            {'name': 'Transform', 'trigger': VolcanoTransform}
+            {'name': 'Basic Attack', 'trigger': attack,'type':'attack','cooldown':0,'cooldownLeft':0},
+            {'name': 'Lava Lash:', 'trigger': VolcanoLavaLash,'type':'attack','cooldown':3,'cooldownLeft':4},
+            {'name': 'Transform', 'trigger': VolcanoTransform,'type':'utility','cooldown':100,'cooldownLeft':101}
         ]
     },
     {
@@ -50,9 +51,9 @@ characters = [
         'hp':75,
         'power':50,
         'skills': [
-            {'name': 'Basic Attack', 'trigger': attack},
-            {'name': 'Destroy', 'trigger': CannonDestroy},
-            {'name': 'Heal', 'trigger': CannonHeal}
+            {'name': 'Basic Attack', 'trigger': attack,'type':'attack','cooldown':0,'cooldownLeft':0},
+            {'name': 'Destroy', 'trigger': CannonDestroy,'type':'attack','cooldown':3,'cooldownLeft':4},
+            {'name': 'Heal', 'trigger': CannonHeal,'type':'utility','cooldown':4,'cooldownLeft':5}
         ]
     }
 ]
@@ -79,20 +80,38 @@ while p1["hp"]>0 and p2["hp"]>0:
             for i,skill in enumerate(value):
                 print(i, skill['name'])
     
-    p1Skill = int(input('Select skill: ')) # 2
+    p1Skill = p1['skills'][int(input('Select skill: '))]
     
-    p1['skills'][p1Skill]['trigger'](p1, p2)
-    
+        
+    if p1Skill['type'] =='attack':
+        p1Skill['trigger'](p1, p2)
+    else:
+        p1Skill['trigger'](p1)
     print(p1['name'], p1['hp'])
     print(p2['name'], p2['hp'])
     
+    if 0>=p2 ['hp']:
+        break
+    print(p2['name'], p2['hp'])
+    for key, value in p2.items():
+        if key == 'skills':
+            for i,skill in enumerate(value):
+                print(i, skill['name'])
+    
+    p2Skill = p2['skills'][int(input('Select skill: '))]
+    if p2Skill['type'] =='attack':
+        p2Skill['trigger'](p2, p1)
+    else:
+        p2Skill['trigger'](p2)
+    print(p2['name'], p2['hp'])
+    print(p1['name'], p1['hp'])
+    
     
     # TODO Select Skill DONE
-    # TODO Solve Heal Attack function problem
+    # TODO Solve Heal Attack function problem Done
     # TODO ADD cooldown to skills
     # TODO Add mana cost to skills
-    
-    break
+
 
 if p1["hp"]>0:
     print(f"WINNER!! :P1 {p1['name']}")
